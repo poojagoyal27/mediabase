dad6abc2f34554a3441d0866635ac44f7f982c84
poojagoyal27 <pgoyal27@mail.fresnostate.edu>
2019-12-18 13:11:39 -0800

    travis.yml change

--- applications/podcasts-pd/src/main/java/com/example/mediabase/podcasts/PodcastMicroserviceApplication.java
+++ applications/podcasts-pd/src/main/java/com/example/mediabase/podcasts/PodcastMicroserviceApplication.java
@@ -1,11 +1,12 @@
 package com.example.mediabase.podcasts;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
+
+
 
 @SpringBootApplication
-public class PodcastMicroserviceApplication  {
+public class PodcastMicroserviceApplication {
 
     public static void main(String... args) {
         SpringApplication.run(PodcastMicroserviceApplication.class, args);
