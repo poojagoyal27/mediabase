44c221dc36322337c86138748c057285be09b055
poojagoyal27 <pgoyal27@mail.fresnostate.edu>
2019-12-18 13:25:53 -0800

    travis.yml change

--- components/ui/src/main/java/com/example/mediabase/podcastsui/PodcastUI.java
+++ components/ui/src/main/java/com/example/mediabase/podcastsui/PodcastUI.java
@@ -18,52 +18,23 @@ package com.example.mediabase.podcastsui;
 
 import java.io.Serializable;
 
-
-
 public class PodcastUI implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-
-    private long id;
-
-    private String director;
     private String title;
-    private int year;
-    private String genre;
-    private int rating;
+    private String description;
+    private String url;
+
+    private Long id;
 
     public PodcastUI() {
     }
 
-    public PodcastUI(String title, String director, String genre, int rating, int year) {
-        this.director = director;
+    public PodcastUI(String title, String description, String url) {
         this.title = title;
-        this.year = year;
-        this.genre = genre;
-        this.rating = rating;
-    }
-
-    public PodcastUI(String director, String title, int year) {
-        this.director = director;
-        this.title = title;
-        this.year = year;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
+        this.description = description;
+        this.url = url;
     }
 
     public String getTitle() {
@@ -74,27 +45,29 @@ public class PodcastUI implements Serializable {
         this.title = title;
     }
 
-    public int getYear() {
-        return year;
+    public String getDescription() {
+        return description;
     }
 
-    public void setYear(int year) {
-        this.year = year;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    public String getGenre() {
-        return genre;
+    public String getUrl() {
+        return url;
     }
 
-    public void setGenre(String genre) {
-        this.genre = genre;
+    public void setUrl(String url) {
+        this.url = url;
     }
 
-    public int getRating() {
-        return rating;
+    public Long getId() {
+        return id;
     }
 
-    public void setRating(int rating) {
-        this.rating = rating;
+    public void setId(Long id) {
+        this.id = id;
     }
 }
+
+
