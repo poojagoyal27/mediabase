291372b2428d6a569baacf6b869513a391019b85
poojagoyal27 <pgoyal27@mail.fresnostate.edu>
2019-12-19 10:33:52 -0800

    travis.yml change

--- applications/mediabase/src/main/java/com/example/mediabase/Application.java
+++ applications/mediabase/src/main/java/com/example/mediabase/Application.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
@@ -11,14 +13,11 @@ import com.example.mediabase.moviesui.ActionServlet;
 import com.example.mediabase.moviesui.MovieClient;
 import com.example.mediabase.podcastsui.PodcastClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 
-    @Value("${movies.ms.url}")
-    private String moviesURL;
 
-    @Value("${podcasts.pd.url}")
-    private String podcastsURL;
 
     public static void main(String... args) {
         SpringApplication.run(Application.class, args);
@@ -29,6 +28,7 @@ public class Application {
         return new ServletRegistrationBean(actionServlet, "/moviefun/*");
     }
 
+    @LoadBalanced
     @Bean
     public RestOperations restOperations() {
         return new RestTemplate();
@@ -36,11 +36,11 @@ public class Application {
 
     @Bean
     public MovieClient movieClient(RestOperations restOperations) {
-        return new MovieClient(moviesURL, restOperations);
+        return new MovieClient("//movies-ms/movies", restOperations);
     }
 
     @Bean
     public PodcastClient podcastClient(RestOperations restOperations) {
-        return new PodcastClient(podcastsURL, restOperations);
+        return new PodcastClient("//podcasts-pd/podcasts", restOperations);
     }
 }
--- applications/movies-ms/src/main/java/com/example/mediabase/movies/MovieMicroserviceApplication.java
+++ applications/movies-ms/src/main/java/com/example/mediabase/movies/MovieMicroserviceApplication.java
@@ -2,8 +2,9 @@ package com.example.mediabase.movies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-
+@EnableEurekaClient
 @SpringBootApplication
 public class MovieMicroserviceApplication  {
 
